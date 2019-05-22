df7f0522becd72e924683eb7f264822d8d1c8d08
Nowy <916116364@qq.com>
2019-05-22 18:42:11 +0800

    appManager提交

---
+++ baselib/src/main/java/com/nowy/baselib/app/BaseApp.kt
@@ -0,0 +1,14 @@
+package com.nowy.baselib.app
+
+/**
+ *
+ * @Package:        com.nowy.baselib.app
+ * @ClassName:      BaseApp
+ * @Description:    java类作用描述
+ * @Author:         Nowy
+ * @CreateDate:     2019/5/22 15:46
+ * @UpdateUser:     Nowy
+ * @UpdateDate:     2019/5/22 15:46
+ * @UpdateRemark:   更新说明
+ * @Version:        1.0
+ */
---
+++ baselib/src/main/java/com/nowy/baselib/manager/AppManager.kt
@@ -0,0 +1,16 @@
+package com.nowy.baselib.manager
+
+/**
+ *
+ * @Package:        com.nowy.baselib.manager
+ * @ClassName:      AppManager
+ * @Description:    java类作用描述
+ * @Author:         Nowy
+ * @CreateDate:     2019/5/22 15:54
+ * @UpdateUser:     Nowy
+ * @UpdateDate:     2019/5/22 15:54
+ * @UpdateRemark:   更新说明
+ * @Version:        1.0
+ */
+class AppManager {
+}
